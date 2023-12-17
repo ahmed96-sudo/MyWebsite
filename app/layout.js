4a3d38464f3081dd652ed0805580f428d11e18bc
ahmed96-sudo <asyd12855@gmail.com>
2023-12-17 22:44:35 +0200

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Header from './_components/Header'
 import Foot from './_components/Foot'
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         {children}
         <Foot />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
